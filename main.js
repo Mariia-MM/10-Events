4baa4c1f7d5308d28adc1f79145ede4512fdebd3
Mariia <mytsko.mariia@gmail.com>
2020-08-19 23:12:29 +0200

    create bottoms and audio

---
+++ main.js
@@ -0,0 +1,39 @@
+// function playAudio() {
+//   audio.play();
+// }
+
+const butQ = document.getElementById(`Q`).addEventListener("click", playAudio1);
+const audQ = document.getElementById("sound1");
+function playAudio1() {
+  audQ.play();
+}
+
+const butW = document.getElementById(`W`).addEventListener("click", playAudio2);
+const audW = document.getElementById("sound2");
+function playAudio2() {
+  audW.play();
+}
+
+const butE = document.getElementById(`E`).addEventListener("click", playAudio3);
+const audE = document.getElementById("sound3");
+function playAudio3() {
+  audE.play();
+}
+
+const butR = document.getElementById(`R`).addEventListener("click", playAudio4);
+const audR = document.getElementById("sound4");
+function playAudio4() {
+  audR.play();
+}
+
+const butT = document.getElementById(`T`).addEventListener("click", playAudio5);
+const audT = document.getElementById("sound5");
+function playAudio5() {
+  audT.play();
+}
+
+const butY = document.getElementById(`Y`).addEventListener("click", playAudio6);
+const audY = document.getElementById("sound6");
+function playAudio6() {
+  audY.play();
+}
